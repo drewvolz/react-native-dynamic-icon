4df19108189643dccc72c35d14f98ba304616a6c
Hawken Rives <hawkrives@gmail.com>
2017-10-24 22:21:00 -0500

    Allow passing any icon name to the module

--- ios/ReactNativeDynamicIcon.m
+++ ios/ReactNativeDynamicIcon.m
@@ -2,11 +2,7 @@
 #import "React/RCTLog.h"
 #import "ReactNativeDynamicIcon.h"
 
-// Original
-#define kDefaultName  @"Default"
-// First alternate
-#define kWindmillName @"Windmill"
-#define kWindmillKey  @"icon_type_windmill"
+#define kDefaultName  @"__default__"
 
 @implementation ReactNativeDynamicIcon
 
@@ -16,29 +12,24 @@
 
 RCT_EXPORT_MODULE()
 
-RCT_EXPORT_METHOD(setIconName:(NSString *)iconName)
-{
+RCT_EXPORT_METHOD(setIconName:(NSString *)iconName) {
   // Not supported
   if ([[UIApplication sharedApplication] supportsAlternateIcons] == NO) {
     RCTLog(@"Alternate icons are not supported");
     return;
   }
+
   // Original icon
   if ([iconName isEqualToString:kDefaultName]) {
       [[UIApplication sharedApplication] setAlternateIconName:nil completionHandler:^(NSError * _Nullable error) {
           RCTLog(@"%@", [error description]);
       }];
   }
-  // First alternate
-  else if ([iconName isEqualToString:kWindmillName]) {
-      [[UIApplication sharedApplication] setAlternateIconName:kWindmillKey completionHandler:^(NSError * _Nullable error) {
-          RCTLog(@"%@", [error description]);
-      }];
-  }
-  // No match
-  else {
-    RCTLog(@"No icon with name %@ was found", iconName);
-  }
+
+  // Custom icon
+  [[UIApplication sharedApplication] setAlternateIconName:iconName completionHandler:^(NSError * _Nullable error) {
+      RCTLog(@"%@", [error description]);
+  }];
 }
 
 @end
